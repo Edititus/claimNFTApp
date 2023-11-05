eb8d4f684da2e0aac97fd4c23969f069987219cf
Edititus <tediomo.et@gmail.com>
2023-11-05 18:56:43 +0100

    Build Entry point

---
+++ App.tsx
@@ -0,0 +1,87 @@
+import {
+  ConnectWallet,
+  embeddedWallet,
+  localWallet,
+  metamaskWallet,
+  rainbowWallet,
+  ThirdwebProvider,
+  trustWallet,
+  useAddress,
+  walletConnect,
+} from "@thirdweb-dev/react-native";
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, useColorScheme, View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import NFT from './components/NFT';
+
+const App = () => {
+
+  return (
+    <ThirdwebProvider
+      activeChain='mumbai'
+      supportedWallets={[
+        metamaskWallet({
+          recommended: true,
+        }),
+        rainbowWallet(),
+        walletConnect({
+          recommended: true,
+        }),
+        embeddedWallet({
+          auth: {
+            // you need to enable EmbeddedWallets under your API Key in your thirdweb dashboard:
+            // https://thirdweb.com/dashboard/settings/api-keys
+            options: ['email', 'google'],
+            // you need to add this deeplink in your allowed `Redirect URIs` under your API Key in your thirdweb dashboard:
+            // https://thirdweb.com/dashboard/settings/api-keys
+            redirectUrl: 'rnstarter://',
+          },
+        }),
+        trustWallet(),
+        localWallet(),
+      ]}
+    >
+      <AppInner />
+    </ThirdwebProvider>
+  );
+};
+
+const AppInner = () => {
+  const address = useAddress();
+  const isDarkMode = useColorScheme() === "dark";
+
+  const textStyles = {
+    color: isDarkMode ? Colors.white : Colors.black,
+    ...styles.heading,
+  };
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View style={styles.view}>
+        <ConnectWallet />
+        {address ? <Text>Welcome!</Text> : <NFT />}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
+
+export default App;
---
+++ components/NFT.tsx
@@ -0,0 +1,70 @@
+import {
+  useAddress,
+  useContract,
+  useNFT,
+  useNFTBalance,
+  Web3Button,
+} from '@thirdweb-dev/react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+
+const NFT = () => {
+  const address = useAddress();
+  const contractAddress = '0x9b9DF73Ea696FF30BF82132677C29C213856bF2A';
+  const { contract } = useContract(contractAddress);
+
+  const { data: nft } = useNFT(contract, 0);
+  const { data: nftBalance, isLoading } = useNFTBalance(contract, address, 0);
+  const nftImage = nft?.metadata.image;
+  return (
+    <ScrollView>
+      <View style={styles.nftCard}>
+        <Text style={styles.title}>{nft?.metadata.name}</Text>
+        <Image style={styles.nftImage} source={{ uri: nftImage?.toString() }} />
+        <Text style={styles.text}>
+          <Text>You own:</Text>
+          <Text>{isLoading ? '0' : nftBalance?.toString()}</Text>
+        </Text>
+        <Web3Button
+          contractAddress={contractAddress}
+          action={(contract) => contract.erc1155.claim(0, 1)}
+        >
+          Claim
+        </Web3Button>
+      </View>
+      <Text style={styles.descriptionText}>Description: </Text>
+      <Text style={styles.descriptionText}>{nft?.metadata.description}</Text>
+    </ScrollView>
+  );
+};
+
+export default NFT;
+
+const styles = StyleSheet.create({
+  nftCard: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignContent: 'center',
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    padding: 10,
+  },
+  text: { fontSize: 24, marginBottom: 20 },
+  nftImage: {
+    height: 450,
+    width: 300,
+    marginBottom: 10,
+    borderRadius: 6,
+  },
+  descriptionText: {
+    fontSize: 14,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 20,
+  },
+});
